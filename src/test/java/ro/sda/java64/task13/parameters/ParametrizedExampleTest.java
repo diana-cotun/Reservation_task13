946b6d9776b180b89e9ebe4acce5437cdb638ebc
StefanD <stefan.durla@gmail.com>
2024-04-11 19:25:29 +0300

    Added Parametrized tests

---
+++ src/test/java/ro/sda/java64/task13/parameters/ParametrizedExampleTest.java
@@ -0,0 +1,115 @@
+package ro.sda.java64.task13.parameters;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Month;
+import java.util.EnumSet;
+import java.util.stream.Stream;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ParametrizedExampleTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"Ana","are","mere"})
+    void verify_not_null_Value(String value){
+        assertNotNull(value,"Value should not be null");
+    }
+
+    @ParameterizedTest
+    @MethodSource("providedStringsIsBlank")
+    void verify_parameterized_with_method(String value, boolean expected){
+        assertEquals(expected, StringUtils.isBlank(value));
+    }
+
+    private static Stream<Arguments> providedStringsIsBlank(){
+        return  Stream.of(
+                Arguments.of(null,true),
+                Arguments.of("",true),
+                Arguments.of("Ana",false),
+                Arguments.of("mere",true)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void verify_not_null_Value(int value){
+        assertNotEquals(0,value,"Value should be different than zero");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void isBlank_ShouldReturnTrueForNullInputs(String input) {
+        assertTrue(StringUtils.isBlank(input));
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void isBlank_ShouldReturnTrueForEmptyStrings(String input) {
+        assertTrue(StringUtils.isBlank(input));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    void isBlank_ShouldReturnTrueForAllTypesOfBlankStrings(String input) {
+        assertTrue(StringUtils.isBlank(input));
+    }
+    @ParameterizedTest
+    @EnumSource(Month.class) // passing all 12 months
+    void getValueForAMonth_IsAlwaysBetweenOneAndTwelve(Month month) {
+        int monthNumber = month.getValue();
+        assertTrue(monthNumber >= 1 && monthNumber <= 12);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Month.class, names = {"APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"})
+    void someMonths_Are30DaysLong(Month month) {
+        final boolean isALeapYear = false;
+        assertEquals(30, month.length(isALeapYear));
+    }
+    @ParameterizedTest
+    @EnumSource(
+            value = Month.class,
+            names = {"APRIL", "JUNE", "SEPTEMBER", "NOVEMBER", "FEBRUARY"},
+            mode = EnumSource.Mode.EXCLUDE)
+    void exceptFourMonths_OthersAre31DaysLong(Month month) {
+        final boolean isALeapYear = false;
+        assertEquals(31, month.length(isALeapYear));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Month.class, names = ".+BER", mode = EnumSource.Mode.MATCH_ANY)
+    void fourMonths_AreEndingWithBer(Month month) {
+        EnumSet<Month> months =
+                EnumSet.of(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+        assertTrue(months.contains(month));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"test,TEST", "tEst,TEST", "Java,JAVA"})
+    void toUpperCase_ShouldGenerateTheExpectedUppercaseValue(String input, String expected) {
+        String actualValue = input.toUpperCase();
+        assertEquals(expected, actualValue);
+    }
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void toUpperCase_ShouldGenerateTheExpectedUppercaseValueCSVFile(
+            String input, String expected) {
+        String actualValue = input.toUpperCase();
+        assertEquals(expected, actualValue);
+    }
+}
